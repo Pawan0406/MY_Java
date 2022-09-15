109d3a7cbdd273d04a2773b69e22d5789d66ac55
Pawan0406 <113007283+Pawan0406@users.noreply.github.com>
2022-09-15 12:39:24 +0530

    Add files via upload

---
+++ PatterN.java
@@ -0,0 +1,22 @@
+class 
+{
+ 	public static void main(String[] args)
+	{
+		for(int i=1;i<=6;i++)
+		{
+		 for(int j=1;j<=6;j++)
+			{
+			           if(j==6||i==j||j==1)
+			           System.out.print("*");
+				   System.out.print("  ");
+			
+				   
+			}
+		  	System.out.println(  );
+
+
+
+                                           }
+
+                            }
+        }
